45e65ca851dcdd2efb592d3c2b2ce1da38d1d05f
rogetsun <songyw163@163.com>
2015-11-12 13:21:42 +0800

    commit

--- src/com/test/ThreadShareList/MyRunnable.java
+++ src/com/test/ThreadShareList/MyRunnable.java
@@ -13,6 +13,10 @@ public class MyRunnable implements Runnable {
 
     private static Log log = LogFactory.getLog("");
     //    public static String[] list = new String[100];
+    /**
+     * 非阻塞线程安全的list.可用于不同现成之间数据共享.
+     * 支持一个线程添加元素,另一个线程移除元素
+     */
     public static ConcurrentLinkedDeque<String> list = new ConcurrentLinkedDeque<String>();
 
     @Override
