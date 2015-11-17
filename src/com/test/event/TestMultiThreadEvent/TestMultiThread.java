1f8787969025515a5388389757cc04370655928a
rogetsun <songyw163@163.com>
2015-11-17 19:24:33 +0800

    commit

---
+++ src/com/test/event/TestMultiThreadEvent/TestMultiThread.java
@@ -0,0 +1,15 @@
+package com.test.event.TestMultiThreadEvent;
+
+/**
+ * Created by uv2sun on 15/11/17.
+ */
+public class TestMultiThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new InitEvent());
+        t.start();
+        for (int i = 0; i < 100; i++) {
+            new Thread(new TriggerEvent("cmd" + i % 5)).start();
+            Thread.sleep(2000);
+        }
+    }
+}
