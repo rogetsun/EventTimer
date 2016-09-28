86bf66d132e3a8829a24c873bee0f3629cbea6b6
rogetsun <songyw163@163.com>
2016-09-28 16:45:08 +0800

    commit

---
+++ src/com/test/event/testMultiThreadHandler/test.java
@@ -0,0 +1,17 @@
+package com.test.event.testMultiThreadHandler;
+
+import com.uv.event.EventUtil;
+import net.sf.json.JSONObject;
+
+/**
+ * Created by uv2sun on 16/9/28.
+ */
+public class test {
+    public static void main(String[] args) {
+        EventUtil.init(5);
+        EventUtil.on("eh", new MyHandler());
+        for (int i = 0; i < 10; i++) {
+            EventUtil.trigger("eh", JSONObject.fromObject("{id:" + i + "}"));
+        }
+    }
+}
